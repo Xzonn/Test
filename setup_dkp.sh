2c0769456f67f174bfdc5480924ca9d37302b922
Xzonn <Xzonn@outlook.com>
2024-09-30 01:06:19 +0800

    6

--- setup_dkp.sh
+++ setup_dkp.sh
@@ -2,14 +2,14 @@ pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver
 pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
 wget https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.xz
 pacman -U devkitpro-keyring.pkg.tar.xz
-echo <<EOF >> /etc/pacman.conf
+cat <<EOF >> /etc/pacman.conf
 [dkp-libs]
 Server = https://pkg.devkitpro.org/packages
 
 [dkp-windows]
 Server = https://pkg.devkitpro.org/packages/windows/$arch/
 EOF
-pacman -Sy
+cat /etc/pacman.conf
 pacman -Syu
 pacman -Sl dkp-libs
 pacman -S nds-dev
